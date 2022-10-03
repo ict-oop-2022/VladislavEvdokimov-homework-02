02a30de19a3722cf2990f6254b05cce80cf917a3
Vladislav Evdokimov <cool.anonymus@bk.ru>
2022-10-03 07:08:42 +0300

    Done!

--- number/number.cpp
+++ number/number.cpp
@@ -1,1 +1,204 @@
 #include "number.h"
+
+
+//конструктор копирования от числа
+number_t::number_t(std::int64_t a) : number(a) {
+}
+
+//оператор присвоения
+number_t &number_t::operator=(std::int64_t a) {
+  number = a;
+  return *this;
+}
+
+//каст числу
+number_t::operator int64_t() const {
+  return number;
+}
+
+//каст bool
+number_t::operator bool() const {
+  return number;
+}
+
+//каст std string
+number_t::operator std::string() const {
+  return std::to_string(number);
+}
+
+//Бинарные арифметические и битовые операторы
+//+, -, *, /, %, &, |, ^, <<, >>
+number_t operator+(const number_t &num1, const number_t &num2) {
+  number_t new_num = num1;
+  new_num.number += num2.number;
+  return new_num;
+}
+
+number_t operator-(const number_t &num1, const number_t &num2) {
+  number_t new_num = num1;
+  new_num.number -= num2.number;
+  return new_num;
+}
+
+number_t &number_t::operator-=(const number_t &num) {
+  number -= num.number;
+  return *this;
+}
+
+number_t &number_t::operator+=(const number_t &num) {
+  number += num.number;
+  return *this;
+}
+
+number_t operator*(const number_t &num1, const number_t &num2) {
+  number_t new_num = num1;
+  new_num.number *= num2.number;
+  return new_num;
+}
+number_t operator/(const number_t &num1, const number_t &num2) {
+  number_t new_num = num1;
+  new_num.number /= num2.number;
+  return new_num;
+}
+number_t operator%(const number_t &num1, const number_t &num2) {
+  number_t new_num = num1;
+  new_num.number %= num2.number;
+  return new_num;
+}
+
+number_t &number_t::operator*=(const number_t &num) {
+  number *= num.number;
+  return *this;
+}
+number_t &number_t::operator/=(const number_t &num) {
+  number /= num.number;
+  return *this;
+}
+number_t &number_t::operator%=(const number_t &num) {
+  number %= num.number;
+  return *this;
+}
+
+number_t operator^(const number_t &num1, const number_t &num2) {
+  number_t new_num = num1;
+  new_num.number ^= num2.number;
+  return new_num;
+}
+number_t operator&(const number_t &num1, const number_t &num2) {
+  number_t new_num = num1;
+  new_num.number &= num2.number;
+  return new_num;
+}
+number_t operator|(const number_t &num1, const number_t &num2) {
+  number_t new_num = num1;
+  new_num.number |= num2.number;
+  return new_num;
+}
+
+number_t &number_t::operator^=(const number_t &num) {
+  number ^= num.number;
+  return *this;
+}
+number_t &number_t::operator&=(const number_t &num) {
+  number &= num.number;
+  return *this;
+}
+number_t &number_t::operator|=(const number_t &num) {
+  number |= num.number;
+  return *this;
+}
+
+number_t operator>>(const number_t &num1, const number_t &num2) {
+  number_t new_num = num1;
+  new_num.number >>= num2.number;
+  return new_num;
+}
+number_t operator<<(const number_t &num1, const number_t &num2) {
+  number_t new_num = num1;
+  new_num.number <<= num2.number;
+  return new_num;
+}
+number_t &number_t::operator>>=(const number_t &num) {
+  number >>= num.number;
+  return *this;
+}
+number_t &number_t::operator<<=(const number_t &num) {
+  number <<= num.number;
+  return *this;
+}
+
+//Унарные операторы
+number_t number_t::operator-() const {
+  return number_t(-number);
+}
+
+number_t number_t::operator+() const {
+  return number_t(number);
+}
+
+bool number_t::operator!() const {
+  return (number == 0);
+}
+
+number_t number_t::operator~() const {
+  return number_t(~number);
+}
+
+//Операторы инкремента и декремента:
+//префикс
+number_t &number_t::operator++() {
+  ++number;
+  return *this;
+}
+number_t &number_t::operator--() {
+  --number;
+  return *this;
+}
+
+//постфикс
+number_t number_t::operator++(int) {
+  number_t temp(number);
+  ++(*this);
+  return temp;
+}
+
+number_t number_t::operator--(int) {
+  number_t temp(number);
+  --(*this);
+  return temp;
+}
+
+//операторы сравнения
+
+bool operator==(const number_t &num1, const number_t &num2) {
+  return (num1.number == num2.number);
+}
+
+bool operator!=(const number_t &num1, const number_t &num2) {
+  return !(num1 == num2);
+}
+
+bool operator>(const number_t &num1, const number_t &num2) {
+  return num1.number > num2.number;
+}
+bool operator>=(const number_t &num1, const number_t &num2) {
+  return num1.number >= num2.number;
+}
+
+bool operator<(const number_t &num1, const number_t &num2) {
+  return num1.number < num2.number;
+}
+bool operator<=(const number_t &num1, const number_t &num2) {
+  return num1.number <= num2.number;
+}
+
+//Операторы вывода в поток и ввода из потока
+
+std::ostream &operator<<(std::ostream &out, const number_t &number) {
+  out << number.number;
+  return out;
+}
+std::istream &operator>>(std::istream &in, number_t &number) {
+  in >> number.number;
+  return in;
+}
--- number/number.h
+++ number/number.h
@@ -4,5 +4,77 @@
 #include <iostream>
 
 class number_t {
+private:
+  std::int64_t number = 0;
 
+public:
+  //конструкторы
+  number_t() = default;
+  number_t(const number_t &copy) = default;
+  number_t(int64_t);
+
+  //оператор присвоения
+  number_t &operator=(int64_t);
+
+  //каст
+  explicit operator int64_t() const;
+  explicit operator bool() const;
+  explicit operator std::string() const;
+
+  //Бинарные арифметические и битовые операторы
+  friend number_t operator+(const number_t &num1, const number_t &num2);
+  friend number_t operator-(const number_t &num1, const number_t &num2);
+
+  number_t &operator+=(const number_t &num);
+  number_t &operator-=(const number_t &num);
+
+  friend number_t operator*(const number_t &num1, const number_t &num2);
+  friend number_t operator/(const number_t &num1, const number_t &num2);
+  friend number_t operator%(const number_t &num1, const number_t &num2);
+
+  number_t &operator*=(const number_t &num);
+  number_t &operator/=(const number_t &num);
+  number_t &operator%=(const number_t &num);
+
+  friend number_t operator^(const number_t &num1, const number_t &num2);
+  friend number_t operator&(const number_t &num1, const number_t &num2);
+  friend number_t operator|(const number_t &num1, const number_t &num2);
+
+  number_t &operator^=(const number_t &num);
+  number_t &operator&=(const number_t &num);
+  number_t &operator|=(const number_t &num);
+
+  friend number_t operator>>(const number_t &num1, const number_t &num2);
+  friend number_t operator<<(const number_t &num1, const number_t &num2);
+
+  number_t &operator>>=(const number_t &num);
+  number_t &operator<<=(const number_t &num);
+
+  //Унарные операторы
+  number_t operator-() const;
+  number_t operator+() const;
+  bool operator!() const;
+  number_t operator~() const;
+
+  //Операторы инкремента и декремента
+  number_t &operator++();
+  number_t &operator--();
+
+  number_t operator++(int);
+  number_t operator--(int);
+
+  //операторы сравнения
+  friend bool operator==(const number_t &num1, const number_t &num2);
+  friend bool operator!=(const number_t &num1, const number_t &num2);
+
+  friend bool operator>(const number_t &num1, const number_t &num2);
+  friend bool operator>=(const number_t &num1, const number_t &num2);
+
+  friend bool operator<(const number_t &num1, const number_t &num2);
+  friend bool operator<=(const number_t &num1, const number_t &num2);
+
+  //Операторы вывода в поток и ввода из потока
+
+  friend std::ostream &operator<<(std::ostream &out, const number_t &number);
+  friend std::istream &operator>>(std::istream &in, number_t &number);
 };
